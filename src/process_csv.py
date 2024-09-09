95aa0af78286cd3c9340090bf9bb3497f7000758
aspio28 <aspiolea28@gmail.com>
2024-09-09 12:06:47 -0400

    feat: add weather history from havana

--- src/process_csv.py
+++ src/process_csv.py
@@ -29,7 +29,3 @@ def process_weather_data(input_file_path, output_file_path):
     filtered_df = df[['datetime', 'temp', 'humidity', 'precipprob', 'snow', 'windspeed', 'visibility', 'conditions', 'description']]  # Filter the columns
     filtered_df = filtered_df.rename(columns = {'datetime': 'date', 'temp':'temperature', 'windspeed':'wind_speed'})  # Rename columns
     filtered_df.to_csv(output_file_path, index=False)
-
-
-
-
